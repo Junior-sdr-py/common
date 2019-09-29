fc0fc2f4c9cd6a7c731f0f31fdb397496e2d4411
Junior-sdr-py <adray0001@gmail.com>
2019-09-29 20:28:45 +0300

    Домашка по SQL

---
+++ relational_database/config.py
@@ -0,0 +1,19 @@
+import os
+
+DATABASE = {
+    "database": "cursor_db",
+    "user": "cursor",
+    "password": "jnrhjqcz",
+    "port": 5432
+}
+
+TEST_DATABASE = {
+    "database": "test_cursor_db",
+    "user": "test_cursor",
+    "password": "test",
+    "port": 5432
+}
+
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+
+FIXTURES_PATH = os.path.join(PROJECT_PATH, "fixtures")
